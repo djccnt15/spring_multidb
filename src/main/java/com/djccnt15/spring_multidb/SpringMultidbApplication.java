105f3773e4f197bc5a1cb38a21a5b25b98378b93
Jay.D <djccnt15@gmail.com>
2025-04-02 22:49:20 +0900

    initiate project

---
+++ src/main/java/com/djccnt15/spring_multidb/SpringMultidbApplication.java
@@ -0,0 +1,13 @@
+package com.djccnt15.spring_multidb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMultidbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMultidbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/djccnt15/spring_multidb/SpringMultidbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djccnt15.spring_multidb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMultidbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
